9206e4f8e1c9e6d02e0a80e3ec03cf604102cd16
ASHMA06 <ashmasulthana06@gmail.com>
2026-02-03 10:24:28 +0530

    initial commit

--- Day 1.c
+++ Day 1.c
@@ -1,14 +1,62 @@
 #include <stdio.h>
+#include <string.h>
+
+#define SIZE 100
+
+char stack[SIZE];
+int top = -1;
+
+void push(char c)
+{
+    stack[++top] = c;
+}
+
+char pop()
+{
+    return stack[top--];
+}
+
+int isMatching(char open, char close)
+{
+    if (open == '(' && close == ')')
+        return 1;
+    if (open == '{' && close == '}')
+        return 1;
+    if (open == '[' && close == ']')
+        return 1;
+    return 0;
+}
+
+int isBalanced(char exp[])
+{
+    for (int i = 0; exp[i] != '\0'; i++)
+    {
+
+        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+            push(exp[i]);
+
+        else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+        {
+            if (top == -1)
+                return 0;
+
+            char open = pop();
+            if (!isMatching(open, exp[i]))
+                return 0;
+        }
+    }
+
+    return top == -1;
+}
 
 int main()
 {
-    int age = 25;
-    float height = 5.8;
-    char grade = 'A';
+    char exp[] = "{[()]}";
 
-    printf("Age: %d\n", age);
-    printf("Height: %.1f\n", height);
-    printf("Grade: %c\n", grade);
+    if (isBalanced(exp))
+        printf("Balanced\n");
+    else
+        printf("Not Balanced\n");
 
     return 0;
 }
